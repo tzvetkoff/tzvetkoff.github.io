4cc0e1d4b3dccb97d90b05e82124870a89629fac
Latchezar Tzvetkoff <latchezar@tzvetkoff.net>
2014-10-06 18:56:38 +0300

    layouts, min.js, 404

--- build.js
+++ build.js
@@ -2,13 +2,7 @@
 
 var
   fs = require('fs'),
-  cssProcessor = require('clean-css'),
   jsProcessor = require('uglify-js'),
-  cssConfig = {
-    keepSpecialComments     : 0,
-    keepBreaks              : false,
-    debug                   : false,
-  },
   jsConfig = {
     fromString              : true,
     copyright               : false,
@@ -18,9 +12,6 @@ var
     unused                  : false,
   };
 
-function css(source) {
-  return new cssProcessor(cssConfig).minify(source);
-}
 function js(source) {
   return jsProcessor.minify(source, jsConfig);
 }
@@ -33,18 +24,6 @@ function write(filename, contents) {
 
 var
   jsDev   = read('assets/javascripts/main.js'),
-  jsMin   = js(jsDev, jsConfig).code,
-
-  ieDev   = read('assets/javascripts/ie.js'),
-  ieMin   = js(ieDev, jsConfig).code,
-
-  cssDev  = read('assets/stylesheets/main.css'),
-  cssMin  = css(cssDev, cssConfig),
-
-  synDev  = read('assets/stylesheets/syntax.css'),
-  synMin  = css(synDev, cssConfig);
+  jsMin   = js(jsDev, jsConfig).code;
 
 write('assets/javascripts/main.min.js', jsMin);
-write('assets/javascripts/ie.min.js', ieMin);
-write('assets/stylesheets/main.min.css', cssMin);
-write('assets/stylesheets/syntax.min.css', synMin);
